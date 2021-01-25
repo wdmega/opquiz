21dc23ea5d040e10062106da9ff1c49f44cc148d
Weslley Daniel <weslleydaniel.programming@outlook.com>
2021-01-25 14:57:22 -0300

    First Comit to OPQuiz

---
+++ pages/index.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widgets';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px){
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+
+export default function Home() {
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+            <Widget.Header>
+            <h1>The Legend of Meme</h1>
+            </Widget.Header>
+
+          <Widget.Content>
+            <p>Lorem ipsum</p>
+          </Widget.Content>
+
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <Widget.Header>
+
+              <h1>The Legend of Meme</h1>
+
+            </Widget.Header>
+
+            <p>Lorem ipsum</p>
+
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/wdmega" />
+    </QuizBackground>
+  )
+}
